248c33bdac4cd5065f01f24a1ba53568667cc563
Siraj Mohammed Saleem <sirajms5@yahoo.com>
2023-04-23 10:13:53 -0400

    made user class for user input

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        User user = new User();
+        char option;
+        String loopControl;
+
+        do {
+            // menu list
+            String message = String.format("%s%n%4s%s%n%4s%s%n", "Choose which type of converter you would like to use:",
+            "",
+            "1. Convert intger number to binary.",
+            "",
+            "2. Convert decimal number to binary and hexadecimal using IEEE-754 Floating point converter.");
+            option = user.inputCharacter(message);
+
+            // menu options
+            switch(option){
+                case '1':
+                    int userInteger = user.inputInterger("Enter integer");
+                    //todo: create class to convert numbers
+                break;
+                case '2':
+                break;
+                default:
+                    System.out.println("Invalid input");
+                break;
+            }
+            loopControl = user.inputString("Would you like to convert another number Y/N: ");
+        } while (loopControl.equalsIgnoreCase("Y"));
     }
 }
---
+++ src/User.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+// User class will ask user for input
+public class User {
+
+    private Scanner keyboard = new Scanner(System.in);
+
+    // inputString methood will request user to input a string and it will return it
+    public String inputString(String message){
+        System.out.print(message);
+        String userInput = keyboard.nextLine();
+        return userInput;
+    }
+
+    // inputCharacter will ask the user to enter a character
+    public char inputCharacter(String message){
+        System.out.printf(message);
+        char userInput = inputCharacter();
+        return userInput;
+    }
+
+    // this method will make sure to select on character
+    private char inputCharacter(){
+        char userinput = keyboard.next().charAt(0);
+        keyboard.nextLine();
+        return userinput;
+    }
+
+    // inputInteger method will request user to input an integer
+    public int inputInterger(String message){
+        System.out.print(message);
+        int userInput = inputInteger();
+        return userInput;
+    }
+
+    // this method will check if input is integer
+    private int inputInteger(){
+        boolean isInputBad = true;
+        int value = 0;
+        while(isInputBad){
+            if(keyboard.hasNextInt()){
+                value = keyboard.nextInt();                
+                keyboard.nextLine();
+                isInputBad = false;
+            } else {
+                System.out.print("Invalid input. Enter an integer number: ");
+            }
+        }
+        return value;
+    }
+}
